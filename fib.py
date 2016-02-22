cf73df7adfdd8a67600ccb986bfa3468a79ab7b7
Hans Fangohr bin <fangohr@soton.ac.uk>
2016-02-22 16:51:34 +0000

    add implementation.

---
+++ fib.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+def fib(n):
+    print("n={}".format(n))
+    res = int(round(
+             ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
+         ))
+    print("res = {}".format(res))
+    return res
