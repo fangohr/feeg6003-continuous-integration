6f34669f8cf168bd9820791512f5d1421229cbeb
Hans Fangohr bin <fangohr@soton.ac.uk>
2016-02-22 16:51:44 +0000

    Updated tests

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -4,5 +4,7 @@ def test_fibonacci():
     """
 
     import fib
-    assert fib.fib(1) == 1
+    assert fib.fib(2) == 1
+    assert fib.fib(3) == 2
     assert fib.fib(10) == 55
+    assert fib.fib(11) == 89
